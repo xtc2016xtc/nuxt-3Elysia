2cf0ce8417c54906e8dffbd91c8225cdf6af62e9
熊天成 <728611827@qq.com>
2024-01-04 21:59:47 +0800

    415

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,12 @@
+<script lang="ts">
+</script>
+
 <template>
   <div>
-    <h1>Hello World!</h1>
+    <h1>Index</h1>
   </div>
 </template>
 
-<script>
-export default {
-  name: 'HelloWorld',
-}
-</script>
-
 <style scoped>
-h1 {
-  color: red;
-}
+
 </style>
