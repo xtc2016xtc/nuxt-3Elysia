66752555951874288c57eab68b78a02a94ecf176
熊天成 <728611827@qq.com>
2024-01-06 10:59:43 +0800

    更新

--- app.vue
+++ app.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="bg-slate-900 h-screen text-white">
-    <!-- 0:37:03 -->
+    <!-- 0:40:04 -->
     <NuxtLayout>
 
       <NuxtPage />
--- pages/custom.vue
+++ pages/custom.vue
@@ -2,35 +2,6 @@
 definePageMeta({
   // Page meta
   layout: 'custom',
-  // Page title
-  title: 'Custom page',
-  // Page description
-  description: 'This is a custom page',
-  // Page language
-  image: 'https://nuxtjs.org/nuxt-card.png',
-  // Page image
-  url: 'https://nuxtjs.org/examples/custom-page',
-  // Page URL
-  twitter: 'nuxt_js',
-  // Twitter card
-  keywords: 'custom, page, nuxt',
-  // Page keywords
-  publishedAt: '2021-01-01',
-  // Published at
-  modifiedAt: '2021-01-01',
-  // Modified at
-  robots: 'index,follow',
-  // Robots
-  customData: 'Custom data',
-  // Custom data
-  customMeta: [
-    {
-      hid: 'customMeta',
-      name: 'customMeta',
-      content: 'Custom meta'
-    }
-  ]
-  // Custom meta
 })
 </script>
 <template>
