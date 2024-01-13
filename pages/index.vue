3d9fd834bd76028c41b4d96a340b8e1a11c8f29e
xtc2016xtc <728611827@qq.com>
2024-01-13 15:53:24 +0800

    gengx

--- pages/index.vue
+++ pages/index.vue
@@ -6,8 +6,9 @@ const { data: products,pending } = await useLazyFetch('/api/products')
 </script>
 
 <template>
-  <div>
-    <p v-for="(item,index) in pending ? products : products" :key="index">{{ item.name }}</p>
+  <div v-for="(item,index) in pending ? products : products" :key="index">
+    <p>{{ item.name }}</p>
+    <p>{{ item.image }}</p>
     <!-- <p>{{ pending ? "加载中": products }}</p> -->
   </div>
 </template>
