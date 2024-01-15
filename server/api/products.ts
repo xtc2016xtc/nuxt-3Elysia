fed6442059cd2720e9294c9db499fa97eea86802
xtc2016xtc <728611827@qq.com>
2024-01-15 09:16:27 +0800

    更新

--- server/api/products.ts
+++ server/api/products.ts
@@ -1,5 +1,10 @@
 import data from './products.json';
-// asdghawg
+/**
+ * @description defineEventHandler is a function that returns a promise
+ * @param {function} callback
+ * @returns {Promise<any>}
+ * @example defineEventHandler(async() => { return new Promise<any>((resolve) => { setTimeout(() => { resolve(data); }, 2000); }) });
+ */
 export default defineEventHandler(async() =>{
   return new Promise<any>((resolve) => {
     setTimeout(() => {
